6815d22319c96cf0a494a82e4362b2ababdad8e4
lchis <lchis@isoftwaresystems.com>
2019-05-09 14:42:42 +0300

    init commit

---
+++ CMS.Web/Controllers/EditorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Web.Controllers
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        public ActionResult Html()
+        {
+            return View();
+        }
+
+        public ActionResult Word()
+        {
+            return View();
+        }
+
+        public ActionResult Excel()
+        {
+            return View();
+        }
+
+        public ActionResult Pdf()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMS.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+
+namespace CMS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                var a = User.Identity.GetUserId();
+            }
+            catch (Exception ex)
+            {
+ 
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
